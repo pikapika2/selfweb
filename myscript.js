45da885601fb67d27a22f1c29a5bd8ceaeb5dca2
pikapika2 <34570780+pikapika2@users.noreply.github.com>
2018-06-01 21:03:17 +0900

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,7 +1,33 @@
 console.log("hello from script")
 
-var z = 18
+var weeks = new Array('日','月','火','水','木','金','土');
 
-var k = z * 2
+var now = new Date();
 
-console.log(k)
+var year = now.getYear(); // 年
+var month = now.getMonth() + 1; // 月
+var day = now.getDate(); // 日
+var week = weeks[ now.getDay() ]; // 曜日
+var hour = now.getHours(); // 時
+var min = now.getMinutes(); // 分
+var sec = now.getSeconds(); // 秒
+
+if(year < 2000) { year += 1900; }
+
+// 数値が1桁の場合、頭に0を付けて2桁で表示する指定
+if(month < 10) { month = "0" + month; }
+if(day < 10) { day = "0" + day; }
+if(hour < 10) { hour = "0" + hour; }
+if(min < 10) { min = "0" + min; }
+if(sec < 10) { sec = "0" + sec; }
+
+// 表示開始
+x = ('現在：' + year + '年' + month + '月' + day + '日（' + week + '）');
+y = (hour + '時' + min + '分' + sec + '秒');
+// 表示終了
+var date = document.createElement('p');
+				var text = document.createTextNode(x);
+				var text1 = document.createTextNode(y);
+				document.body.appendChild(date).appendChild(text);
+				document.body.appendChild(date).appendChild(text1);
+
